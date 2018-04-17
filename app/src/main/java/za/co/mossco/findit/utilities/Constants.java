bfdc43969d24f6df0bcd9555699d08268d3843b3
mosesmoabelo <kgoputsomoses.moabelo@absa.co.za>
2018-04-17 20:14:07 +0200

    Add Constants class for holding constants variables

---
+++ app/src/main/java/za/co/mossco/findit/utilities/Constants.java
@@ -0,0 +1,7 @@
+package za.co.mossco.findit.utilities;
+
+public class Constants {
+    public static final String BASE_URL = "https://api.foursquare.com/v2/";
+    public static final String API_KEY = "";
+    public static final String APP_SECRET_KEY = "";
+}
