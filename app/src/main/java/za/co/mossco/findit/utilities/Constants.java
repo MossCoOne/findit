8bc6bae3b7cfbe096c8d7c55a05513856462f587
mosesmoabelo <kgoputsomoses.moabelo@absa.co.za>
2018-04-18 21:44:56 +0200

    Add the MainLayout UI

--- app/src/main/java/za/co/mossco/findit/utilities/Constants.java
+++ app/src/main/java/za/co/mossco/findit/utilities/Constants.java
@@ -2,6 +2,6 @@ package za.co.mossco.findit.utilities;
 
 public class Constants {
     public static final String BASE_URL = "https://api.foursquare.com/v2/";
-    public static final String API_KEY = "";
-    public static final String APP_SECRET_KEY = "";
+    public static final String API_KEY = "SD3P401ATIGHOD2BJHJEBPDNNCG5RZ5MP5NSJP0RNQBRUJOA";
+    public static final String APP_SECRET_KEY = "UZ3FLDZSJNWLKNCISKNOFU42PHFDNUNXPCFVSPCHO4MCT04Y";
 }
