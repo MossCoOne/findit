a3074e1caebde9664766025370dcfc3ccfda948c
mosesmoabelo <kgoputsomoses.moabelo@absa.co.za>
2018-04-18 18:01:27 +0200

    Add the beans

---
+++ app/src/main/java/za/co/mossco/findit/bean/Response.java
@@ -0,0 +1,20 @@
+package za.co.mossco.findit.bean;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Response {
+    @SerializedName("venues")
+    @Expose
+    private List<Venue> venues = null;
+
+    public List<Venue> getVenues() {
+        return venues;
+    }
+
+    public void setVenues(List<Venue> venues) {
+        this.venues = venues;
+    }
+}
