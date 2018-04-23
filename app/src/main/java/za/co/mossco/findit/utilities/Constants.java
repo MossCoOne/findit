1307ebbbeacb86873c5d6302f368671f821813d2
mosesmoabelo <kgoputsomoses.moabelo@absa.co.za>
2018-04-23 12:32:16 +0200

    Update constants

--- app/src/main/java/za/co/mossco/findit/utilities/Constants.java
+++ app/src/main/java/za/co/mossco/findit/utilities/Constants.java
@@ -1,7 +1,8 @@
 package za.co.mossco.findit.utilities;
 
 public class Constants {
-    public static final String BASE_URL = "https://api.foursquare.com/v2/";
-    public static final String API_KEY = "SD3P401ATIGHOD2BJHJEBPDNNCG5RZ5MP5NSJP0RNQBRUJOA";
-    public static final String APP_SECRET_KEY = "UZ3FLDZSJNWLKNCISKNOFU42PHFDNUNXPCFVSPCHO4MCT04Y";
+    public static final String GOOGLE_BASE_URL = "https://maps.googleapis.com/maps/api/place/";
+    public static final String GOOGLE_API_KEY = "";
+    public static final double RADIUS = 10000;
+
 }
